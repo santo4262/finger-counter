24bde579ff7e29f4a708c5b00920f56505c125b6
santo4262 <vtu28409@veltech.edu.in>
2026-06-19 16:03:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,58 +16,95 @@ hands.setOptions({
 
 hands.onResults(onResults);
 
+// Store previous count so speech only happens when count changes
+let lastCount = -1;
+
+// Text-to-Speech Function
+function speakCount(count) {
+  if (count === lastCount) return;
+
+  lastCount = count;
+
+  // Stop any ongoing speech
+  window.speechSynthesis.cancel();
+
+  const messages = [
+    "Zero fingers detected",
+    "One finger detected",
+    "Two fingers detected",
+    "Three fingers detected",
+    "Four fingers detected",
+    "Five fingers detected"
+  ];
+
+  const speech = new SpeechSynthesisUtterance(messages[count]);
+
+  speech.lang = "en-US";
+  speech.rate = 1;
+  speech.pitch = 1;
+
+  window.speechSynthesis.speak(speech);
+}
+
+// Finger Counting Logic
 function countFingers(landmarks) {
   let count = 0;
 
   // Thumb
   if (landmarks[4].x < landmarks[3].x) count++;
 
-  // Index
+  // Index Finger
   if (landmarks[8].y < landmarks[6].y) count++;
 
-  // Middle
+  // Middle Finger
   if (landmarks[12].y < landmarks[10].y) count++;
 
-  // Ring
+  // Ring Finger
   if (landmarks[16].y < landmarks[14].y) count++;
 
-  // Pinky
+  // Pinky Finger
   if (landmarks[20].y < landmarks[18].y) count++;
 
   return count;
 }
 
+// Process MediaPipe Results
 function onResults(results) {
-  if (results.multiHandLandmarks &&
-      results.multiHandLandmarks.length > 0) {
-
+  if (
+    results.multiHandLandmarks &&
+    results.multiHandLandmarks.length > 0
+  ) {
     const landmarks = results.multiHandLandmarks[0];
     const fingers = countFingers(landmarks);
 
+    // Update count on screen
     countElement.innerText = fingers;
+
+    // Speak count
+    speakCount(fingers);
   }
 }
 
-navigator.mediaDevices.getUserMedia({
-  video: true
-})
-.then(function(stream) {
+// Start Webcam
+navigator.mediaDevices
+  .getUserMedia({
+    video: true
+  })
+  .then(function (stream) {
+    videoElement.srcObject = stream;
 
-  videoElement.srcObject = stream;
+    const camera = new Camera(videoElement, {
+      onFrame: async () => {
+        await hands.send({
+          image: videoElement
+        });
+      },
+      width: 640,
+      height: 480
+    });
 
-  const camera = new Camera(videoElement, {
-    onFrame: async () => {
-      await hands.send({
-        image: videoElement
-      });
-    },
-    width: 640,
-    height: 480
+    camera.start();
+  })
+  .catch(function (error) {
+    console.log("Camera Error:", error);
   });
-
-  camera.start();
-
-})
-.catch(function(error) {
-  console.log("Camera Error:", error);
-});
